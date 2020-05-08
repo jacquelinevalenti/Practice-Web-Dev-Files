800bb4d9864e1479447affbc3880900d7ad7c833
Jackie Valenti <jacquelinevalenti@outlook.com>
2020-05-08 14:18:14 -0700

    addition of JS practice files

---
+++ stalker.js
@@ -0,0 +1,7 @@
+let firstName = prompt("What is your first name?");
+let lastName = prompt("What is your last name?");
+let userAge = prompt("What is your age?");
+console.log("Your full name is " + firstName + " " + lastName);
+console.log("You are " + userAge + " years old");
+alert("Hello " + firstName + " " + lastName + "!");
+alert("You are " + userAge + " years old");
