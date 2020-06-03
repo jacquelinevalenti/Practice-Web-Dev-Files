910a8f180223520d118a90b0007d51831e76ee39
Jackie Valenti <jacquelinevalenti@outlook.com>
2020-06-02 17:52:21 -0700

    updates to JS to-do list
    added delete functionality, refactored code

--- todo.js
+++ todo.js
@@ -9,12 +9,15 @@ let userchoice = prompt("What would you like to do?");
 while (userchoice !== "quit"){
     // add a new to-do
     if(userchoice === "new"){
-        let usertodo = prompt("Please enter your to-do: ");
-        todos.push(usertodo);
+        addToDo();
     }
     // list the to-dos
     else if(userchoice === "list"){
-        console.log(todos);
+        listToDos();
+    }
+    // remove a to-do
+    else if(userchoice === "delete"){
+        deleteToDo();
     }
     // handle for invalid entries from user
     else{
@@ -22,6 +25,26 @@ while (userchoice !== "quit"){
     }
     // ask for input again to continue the loop
     userchoice = prompt("What would you like to do?");
+}
+
+function listToDos(){
+    console.log("******");
+    todos.forEach(function(thing, i){
+        console.log(i + ": " + thing);
+        })
+    console.log("******");
+}
+
+function deleteToDo(){
+    let deleted = prompt("Which index should be removed?");
+    todos.splice(deleted, 1);
+    console.log("To-do deleted");
+}
+
+function addToDo(){
+    let usertodo = prompt("Please enter your to-do: ");
+    todos.push(usertodo);
+    console.log("To-do added");
 }
 
 // acknowledge that the loop has exited
