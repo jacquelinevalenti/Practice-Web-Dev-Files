4b252e298e08ce59abfe4b7293a9b8309412ee3a
Jackie Valenti <jacquelinevalenti@outlook.com>
2020-06-05 17:32:18 -0700

    initial commit of practice jQuery methods

---
+++ jQueryDemoMethods.js
@@ -0,0 +1,15 @@
+
+// change image width to 200px
+$("img").css("width", "200px");
+
+// below would change all image sources to scary pine martin photo
+// $("img").attr("src", "https://c3.staticflickr.com/3/2418/2243463214_f32ab004af_b.jpg");
+
+// this only changes the first image to scary pine martin
+$("img:first-of-type").attr("src", "https://c3.staticflickr.com/3/2418/2243463214_f32ab004af_b.jpg");
+
+// this changes the input type from text to checkbox
+// $("input").attr("type", "checkbox");
+
+// this changes the last image to scary pine martin
+$("img").last().attr("src", "https://c3.staticflickr.com/3/2418/2243463214_f32ab004af_b.jpg");
